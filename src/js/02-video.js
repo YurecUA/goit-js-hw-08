801524c23c8db2f1b134154c410b71b910d84f9e
YurecUA <81627173+YurecUA@users.noreply.github.com>
2022-11-15 18:35:45 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
+const setTime = Number(localStorage.getItem(STORAGE_KEY));
 
 const throttle = require('lodash.throttle', { 'trailing': false });
 
@@ -13,4 +14,5 @@ function onPlay(data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(setTime);
+
